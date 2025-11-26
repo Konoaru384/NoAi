1f57d8aa9479d3d7f6cdf94c0655efb2d400e6db
Konoaru <jordan.jeanson31470@gmail.com>
2025-11-26 16:04:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("NO-AI est prêt à ridiculiser les IA !");
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const loader = document.getElementById("loader-screen");
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 1800);
+});
